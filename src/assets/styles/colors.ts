23329d314f5c7ba808250136d5ff61b8012ca7bc
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2023-01-23 13:54:54 -0300

    colors-constants-mixin

--- src/assets/styles/colors.ts
+++ src/assets/styles/colors.ts
@@ -1,6 +1,6 @@
 export const colors = {
-  primary: "#6CEA69",
-  secondary: "#E26B6B",
+  primaryColor: "#6CEA69",
+  secondaryColor: "#E26B6B",
 
   primaryOpacity: "rgba(108, 234, 105, 0.4)",
   shadowColor: "#6CEA69",
