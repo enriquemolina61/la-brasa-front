d5f94210720f3511eeae6c76fbdcd00f6572d9df
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2023-01-27 20:58:48 -0300

    login

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { RoutesPath } from "./types/routes";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 const Router = () => {
   return (
     <Routes>
+      <Route path={RoutesPath.LOGIN} element={<Login />} />
       <Route path={RoutesPath.HOME} element={<Home />} />
     </Routes>
   );
